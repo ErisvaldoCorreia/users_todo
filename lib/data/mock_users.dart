aa16e0f835853f2a8d736491a766d024b925575b
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-10-19 08:42:51 -0300

    criando modelo e mock de users

---
+++ lib/data/mock_users.dart
@@ -0,0 +1,22 @@
+import 'package:users_todo/models/user.dart';
+
+const MOCK_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Erisvaldo',
+      email: 'erisvaldo@email.com',
+      avatarUrl:
+          'https://www.pngkey.com/png/full/115-1150420_avatar-png-pic-male-avatar-icon-png.png'),
+  '2': const User(
+      id: '2',
+      name: 'Kyriakis',
+      email: 'erisvaldo@email.com',
+      avatarUrl:
+          'https://www.pngkey.com/png/full/115-1150420_avatar-png-pic-male-avatar-icon-png.png'),
+  '3': const User(
+      id: '3',
+      name: 'Junior',
+      email: 'erisvaldo@email.com',
+      avatarUrl:
+          'https://www.pngkey.com/png/full/115-1150420_avatar-png-pic-male-avatar-icon-png.png'),
+};
