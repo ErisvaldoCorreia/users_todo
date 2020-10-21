7c5ff23b796ac7a7377fb43f2cd3437da0bed31c
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-10-21 08:38:17 -0300

    apenas amplia mock

--- lib/data/mock_users.dart
+++ lib/data/mock_users.dart
@@ -14,14 +14,25 @@ const MOCK_USERS = {
       avatarUrl:
           'https://www.pngkey.com/png/full/115-1150420_avatar-png-pic-male-avatar-icon-png.png'),
   '3': const User(
-      id: '3',
-      name: 'Junior',
-      email: 'erisvaldo@email.com',
-      avatarUrl:
-          'https://www.pngkey.com/png/full/115-1150420_avatar-png-pic-male-avatar-icon-png.png'),
+    id: '3',
+    name: 'Junior',
+    email: 'erisvaldo@email.com',
+  ),
   '4': const User(
     id: '4',
     name: 'Sem Avatar',
     email: 'erisvaldo@email.com',
   ),
+  '5': const User(
+      id: '5',
+      name: 'Novo User',
+      email: 'erisvaldo@email.com',
+      avatarUrl:
+          'https://www.pngkey.com/png/full/115-1150420_avatar-png-pic-male-avatar-icon-png.png'),
+  '6': const User(
+      id: '6',
+      name: 'Mais um user',
+      email: 'erisvaldo@email.com',
+      avatarUrl:
+          'https://www.pngkey.com/png/full/115-1150420_avatar-png-pic-male-avatar-icon-png.png'),
 };
